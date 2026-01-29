0b8e30e1be947241a6d859047c8410c3256dc3b5
john <john.diri10@gmail.com>
2026-01-29 09:54:37 -0700

    Add Basic Express Server with a test /health endpoint

---
+++ BackEnd/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/health", (req,res) => {
+    res.send("API Running");
+});
+
+app.listen(port,() => {
+    console.log(`Server listening on ${port}`);
+});
